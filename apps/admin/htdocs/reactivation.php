8ddaa95a0f9305aa3ab8d1f426de1999d7528f91
abellimz <abellim1309@gmail.com>
2017-04-04 13:37:00 +0800

    Remove redundant admin side bar for all pages

--- apps/admin/htdocs/reactivation.php
+++ apps/admin/htdocs/reactivation.php
@@ -68,14 +68,6 @@
   <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar" style="height:auto;">
-      <!-- Sidebar user panel -->
-      <div class="user-panel">
-        <div class="pull-left image">
-        </div>
-        <div class="pull-left info">
-          <p>Admin</p>
-        </div>
-      </div>
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu">
         <li class="header">NAVIGATION</li>
