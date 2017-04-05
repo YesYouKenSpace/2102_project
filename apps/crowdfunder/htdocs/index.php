6be3a5156b414a7e381e345f67e44621394ae58b
Muhammad Hamsyari <hamsyari22@gmail.com>
2017-04-05 20:47:19 +0800

    Add main page for application

--- apps/crowdfunder/htdocs/index.php
+++ apps/crowdfunder/htdocs/index.php
@@ -1,151 +1,118 @@
-<?php
-session_start();
-
-if(isset($_SESSION['usr_id'])!="") {
-    if ($_SESSION['usr_role'] === 1) {
-      header("Location: admin/dashboard.php");
-    } else {
-      header("Location: user/index.php");
-    }
-}
-
-$dbconn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=postgres")
-or die('Could not connect: ' . pg_last_error());
-
-//check if form is submitted
-if (isset($_POST['login'])) {
-
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $query = "SELECT * FROM member WHERE email = '".$_POST['email']."' AND password = crypt('".$_POST['password']."', password)";
-    $result = pg_query($query) or die('Query failed: ' . pg_last_error()); 
-
-    if ($row = pg_fetch_array($result)) {
-        $_SESSION['usr_id'] = $row['email'];
-        $_SESSION['usr_role'] = $row['roleid'];
-        if ($_SESSION['usr_role'] == 1) {
-          header("Location: index.php");
-        } else {
-          header("Location: user/index.php");
-        }
-    } else {
-        $errormsg = "Incorrect Email or Password!!!";
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Login</title>
-
-    <!-- Bootstrap -->
+	<head>
+		<title>Crowdfunder</title>
+		<!-- Bootstrap core CSS -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-    
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-  
-    <!-- DataTables -->
-    <link rel="stylesheet" href="plugins/datatables/dataTables.bootstrap.css">
-  
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
     <!-- Custom styles for this template -->
     <link href="main.css" rel="stylesheet">
-    
-    
-  </head>
+	</head>
+	<body>
+		<?php
+			$dbconn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=postgres")
+	    				or die('Could not connect: ' . pg_last_error());
+		?>
+		<div class="wrapper" style="height: auto;">
+	    	<header class="main-header">
+			    <a href="index.php" class="logo logouser">
+			      <span class="logo-lg"><b>CrowdFunder</b></span>
+			    </a>
+	    		<nav class="navbar navbaruser navbar-static-top">
+	          		<a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+		            	<span class="sr-only">Toggle navigation</span>
+		          	</a>
+		          	<div class="navbar-custom-menu">
+		            	<ul class="nav navbar-nav">
+		              		<li class="user user-menu">
+		                		<a href="login.php">
+		                  			<span class="hidden-xs">Login</span>
+		                		</a>
+		              		</li>
+	            		</ul>
+	          		</div>
+	        	</nav>
+	  		</header>
+	  		<section id="home" class="parallax-section">
+    				<div class="container" style="position: relative;">
+	      				<div class="row">
+      						<div>
+              					<h1>Crowdfunder</h1>		              
+	          				</div>
+						</div>
+      				</div>
+			</section>
+			<section id="projects-summary">
+				<div class="row">
+					<div class="col-md-2">Children</div>
+					<div class="col-md-2">Education</div>
+					<div class="col-md-2">Humanitarian Assistance</div>
+					<div class="col-md-2">Sports</div>
+					<div class="col-md-2">Technology</div>
+					<div class="col-md-2">Many More...</div>
+				</div>
+			</section>
+	  		<section id="crowdfunder-summary">
+				<div class="container">
+					<div id="counters" class="row count-wrapper">
+      					<div class="col-lg-4">
+	      					<h1>$<span id="count1"></span></h1>
+	      					<span>Funded</span>
+      					</div>
+      					<div class="col-lg-4">
+      						<h1 id="count2">0</h1>
+      						<span>Donors</span>
+      					</div>
+      					<div class="col-lg-4">
+      						<h1 id="count3">0</h1>
+      						<span>Projects</span>
+      					</div>
+					</div>
+				</div>
+			</section>
+	  	</div>
 
-<body>
 
-<div class="wrapper" style="height: auto;">
-    
-    <header class="main-header">
+		<script src="plugins/jQuery/jquery-2.2.3.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+		<script src="plugins/countUp.js"></script>
+		<script>
+	  		$(document).ready(function(){
+	  			var hasCounters = $('#counters').hasClass('count-wrapper');
+			    if (hasCounters) {
+			        var options = {
+			                    useEasing : true,
+			                    useGrouping : true, 
+			                    separator : ','
+			                };
+	                <?php
+	                	$query = "SELECT SUM(t.amount) AS totalAmount FROM Trans t
+								  WHERE t.softdelete = false";
+						$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+						$totalAmount = pg_fetch_assoc($result);
 
-    <!-- Logo -->
-    <a href="index.php" class="logo">
-      <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>CrowdFunder</b></span>
-    </a>
+						$query = "SELECT COUNT(m.email) AS memberCount FROM Member m
+								  WHERE m.softdelete = false";
+						$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+						$memberCount = pg_fetch_assoc($result);
 
-    <!-- Header Navbar: style can be found in header.less -->
-    <nav class="navbar navbar-static-top">
-      <!-- Sidebar toggle button-->
-      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-        <span class="sr-only">Toggle navigation</span>
-      </a>
-      <!-- Navbar Right Menu -->
-      <div class="navbar-custom-menu">
-        <ul class="nav navbar-nav">
-          <!-- User Account: style can be found in dropdown.less -->
-          <li class="dropdown user user-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <span class="hidden-xs"></span>
-            </a>
-          </li>
-        </ul>
-      </div>
-
-    </nav>
-  </header>
-<!-- Left side column. contains the logo and sidebar -->
-  <aside class="main-sidebar">
-    <!-- sidebar: style can be found in sidebar.less -->
-    <section class="sidebar" style="height:auto;">
-      <!-- Sidebar user panel -->
-      
-  </aside>
-    <div class="content-wrapper" style="min-height:916px;">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-      </h1>
-    </section>
-
-    <section class="content">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4 well">
-                <form role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="loginform">
-                    <fieldset>
-                        <legend>Login</legend>
-                        <div class="form-group">
-                            <label for="name">Email</label>
-                            <input type="text" name="email" placeholder="Your Email" required class="form-control" />
-                        </div>
-
-                        <div class="form-group">
-                            <label for="name">Password</label>
-                            <input type="password" name="password" placeholder="Your Password" required class="form-control" />
-                        </div>
-
-                        <div class="form-group">
-                            <input type="submit" name="login" value="Login" class="btn btn-primary" />
-                        </div>
-                    </fieldset>
-                </form>
-                <span class="text-danger"><?php if (isset($errormsg)) { echo $errormsg; } ?></span>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4 text-center">    
-            New User? <a href="register.php">Sign Up Here</a>
-            </div>
-    </section>
-    </div>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-    <!-- DataTables -->
-    <script src="plugins/datatables/jquery.dataTables.min.js"></script>
-    <script src="plugins/datatables/dataTables.bootstrap.min.js"></script>
-    <script src="plugins/bootbox.min.js"></script>
-</body>
+						$query = "SELECT COUNT(p.id) AS projcount FROM Project p
+								  WHERE p.softdelete = false";
+						$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+						$projectCount = pg_fetch_assoc($result);
+					?>
+	                // Counter 1
+	                var counter1 = new CountUp('count1', 0, <?php print_r($totalAmount['totalamount']);?>, 0, 3, options);
+	                counter1.start();
+	                // Counter 2
+	                var counter2 = new CountUp('count2', 0, <?php print_r($memberCount['membercount']);?>, 0, 3, options);
+	                counter2.start();
+	                // Counter 3
+	                var counter3 = new CountUp('count3', 0, <?php print_r($projectCount['projcount']);?>, 0, 3, options);
+	                counter3.start();
+			    }
+	  		});
+	  	</script>
+	</body>
 </html>
